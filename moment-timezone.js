414f6665206959c21d9e43de66a7efb67a9b24d4
Berkay Aydın <sbayd@users.noreply.github.com>
2018-05-09 22:46:07 +0300

    Update moment-timezone.js

--- moment-timezone.js
+++ moment-timezone.js
@@ -17,7 +17,9 @@
 	}
 }(this, function (moment) {
 	"use strict";
-
+	if (moment.version === undefined) {
+		moment = moment.default;
+	}
 	// Do not load moment-timezone a second time.
 	// if (moment.tz !== undefined) {
 	// 	logError('Moment Timezone ' + moment.tz.version + ' was already loaded ' + (moment.tz.dataVersion ? 'with data from ' : 'without any data') + moment.tz.dataVersion);
